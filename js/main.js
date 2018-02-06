27a2a1bac323d4f5dc254b4f74b1da4ef41a13c1
Danielle Espinoza <dvespinoza@Danielles-MacBook-Pro.local>
2018-02-05 20:56:57 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -23,18 +23,28 @@ var cards = [
 ];
 var cardsInPlay = [];
 var checkForMatch = function(){
+  this.setAttribute('src', cards[cardId].cardImage);
   if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
   } else {
   console.log("Sorry, try again.");
   }
 };
-var flipCard = function(cardId){
+var flipCard = function(){
+  this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit)
   checkForMatch();
 };
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+createBoard();
