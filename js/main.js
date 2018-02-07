bc631981021fd3a840f010b7b95f31754e65b2f6
Danielle Espinoza <dvespinoza@Danielles-MacBook-Pro.local>
2018-02-06 22:46:35 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -3,45 +3,50 @@ var cards = [
   {
     rank: "queen",
     suit: "hearts",
-    cardImage: "images/queen-of-hearts.png",
+    cardImage: "images/queen-of-hearts.png"
   },
   {
     rank: "queen",
     suit: "diamonds",
-    cardImage: "images/queen-of-diamonds.png",
+    cardImage: "images/queen-of-diamonds.png"
   },
   {
     rank: "king",
     suit: "hearts",
-    cardImage: "images/king-of-hearts.png",
+    cardImage: "images/king-of-hearts.png"
   },
   {
     rank: "king",
     suit: "diamonds",
-    cardImage:"images/king-of-diamonds.png",
+    cardImage:"images/king-of-diamonds.png"
   }
 ];
 var cardsInPlay = [];
-var checkForMatch = function(){
-  this.setAttribute('src', cards[cardId].cardImage);
+
+function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+    alert("You found a match!");
   } else {
-  console.log("Sorry, try again.");
+    alert("Sorry, try again.");
   }
 };
-var flipCard = function(){
-  this.getAttribute('data-id');
+
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardImage);
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit)
-  checkForMatch();
+  console.log(cards[cardId].suit);
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
 };
-var createBoard = function(){
+
+function createBoard() {
   for (var i = 0; i < cards.length; i++){
     var cardElement = document.createElement('img');
-    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('src', 'images/back.png');
     cardElement.setAttribute('data-id', i);
     cardElement.addEventListener('click', flipCard);
     document.getElementById('game-board').appendChild(cardElement);
